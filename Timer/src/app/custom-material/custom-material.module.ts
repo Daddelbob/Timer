61d669abf41d8ef72f6a691833a3c01680fdf7f9
Severin Klug <Severin.Klug@gmx.de>
2018-08-16 22:14:54 +0200

    feat(material): add material elements

--- Timer/src/app/custom-material/custom-material.module.ts
+++ Timer/src/app/custom-material/custom-material.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonToggleModule } from '../../../node_modules/@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatInputModule,
+    MatIconModule
   ],
-  declarations: [
-  ],
+  declarations: [],
   exports: [
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
-export class CustomMaterialModule { }
+export class CustomMaterialModule {}
--- Timer/src/app/start/start.component.ts
+++ Timer/src/app/start/start.component.ts
@@ -10,12 +10,20 @@ export class StartComponent implements OnInit {
   public countingUp = true;
   public selectedCountMode = 'countDown';
   public selectedTotalSeconds = 5;
+
+  public nightMode = false;
+  public minBrightness = 1;
+  public maxBrightness = 255;
+  public brightnessStep = 1;
+  public selectedBrightness = 200;
+
   timer: any;
   constructor() {}
 
   ngOnInit() {}
 
   tick() {
+    console.log(this.selectedBrightness);
     if (this.time < this.selectedTotalSeconds) {
       this.time++;
     } else {
@@ -30,4 +38,27 @@ export class StartComponent implements OnInit {
   setHours() {}
   setMinutes() {}
   setSeconds() {}
+
+  toggleNightMode() {
+    // const color = document.getElementById('mydiv').style.color;
+    // const backgroundColor = document.getElementById('mydiv').style
+    //   .backgroundColor;
+    // if (color === 'black' && backgroundColor === 'white') {
+    //   document.getElementById('mydiv').style.color = 'white';
+    //   document.getElementById('mydiv').style.backgroundColor = 'black';
+    // } else {
+    //   document.getElementById('mydiv').style.color = 'black';
+    //   document.getElementById('mydiv').style.backgroundColor = 'white';
+    // }
+  }
+
+  adjusteNightMode(event: any) {
+    this.selectedBrightness = event.value;
+    document.getElementById('mydiv').style.color = `rgb(${256 -
+      this.selectedBrightness}, ${256 - this.selectedBrightness}, ${256 -
+      this.selectedBrightness})`;
+    document.getElementById('mydiv').style.backgroundColor = `rgb(${
+      this.selectedBrightness
+    }, ${this.selectedBrightness}, ${this.selectedBrightness})`;
+  }
 }
